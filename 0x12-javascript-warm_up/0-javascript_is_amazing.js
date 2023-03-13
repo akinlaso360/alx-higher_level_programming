475918356eae79a15ea5d21aab9a0436421230c1
akinlaso360 <akinlasonasirudeen@gmail.com>
2023-03-13 09:10:35 -0700

    Tasks

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node 
 
-//print JavaScript is amazing
+// print JavaScript is amazing
 
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
